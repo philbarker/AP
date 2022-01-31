30e374a16897f6f9a57f4ba6658bb758a47b5c35
Phil Barker <phil.barker@pjjk.co.uk>
2022-01-31 14:03:40 +0000

    bump ver

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="AP",
-    version="0.2.3",
+    version="0.2.4",
     description="Application profile data classes",
     author="Phil Barker",
     author_email="phil@pjjk.net",
